a142ff78a5d741538c19f89fd52d0dba33958536
Kazuhiro Yamada <kyamada@sonix.asia>
2019-10-17 21:21:28 +0900

    Add lambda stack

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,10 +1,18 @@
-import sns = require('@aws-cdk/aws-sns');
-import subs = require('@aws-cdk/aws-sns-subscriptions');
-import sqs = require('@aws-cdk/aws-sqs');
-import cdk = require('@aws-cdk/core');
+import sns = require("@aws-cdk/aws-sns");
+import subs = require("@aws-cdk/aws-sns-subscriptions");
+import sqs = require("@aws-cdk/aws-sqs");
+import cdk = require("@aws-cdk/core");
+import lambda = require("@aws-cdk/aws-lambda");
 
 export class CdkWorkshopStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    // defines an AWS Lambda resource
+    const hello = new lambda.Function(this, "HelloHandler", {
+      runtime: lambda.Runtime.NODEJS_8_10,
+      code: lambda.Code.asset("lambda"),
+      handler: "hello.handler"
+    });
   }
 }
